9423c7848915d8c0417539d28e3d83c80b569854
Vũ Ngọc Hoàng <vu.ngochoang2808@gmail.com>
2024-06-20 22:21:36 +0700

    lần đầu

---
+++ config.js
@@ -0,0 +1,14 @@
+const nameGirl = "";
+const giftUrl = "";
+const eventName = "Chúc Mừng emiu đã tốt nghiệp";
+const titleCard = "Tặng emiu";
+const contentCard =
+  "Chúc emiu của anh sau này luôn đạt nhiều thành công và luôn bên anh";
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ:
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = "hot-girl.png";
+const base64 = "";
+const giftImageBase64 = "data:image/png;base64, " + base64;
